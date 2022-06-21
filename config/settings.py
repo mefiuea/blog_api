63d28b2d50f6fb51aafa5d8124d868b7b52014aa
Maciej Koczot <mefiuea@gmail.com>
2022-06-21 13:03:39 +0200

    install psycopg2, setup database settings in settings.py

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': 'db_postgres',
+        # 'HOST': '127.0.0.1',
+        'NAME': os.environ.get('POSTGRES_DATABASE_NAME'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': os.environ.get('POSTGRES_DATABASE_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': 5432,
     }
 }
 
