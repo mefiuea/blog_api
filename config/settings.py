3e5ea0dbd4b27551eb7939413332d6ca0904b1ed
Maciej Koczot <mefiuea@gmail.com>
2022-06-22 15:20:32 +0200

    upgrade settings and urls

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
