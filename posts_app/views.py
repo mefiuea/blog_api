1dbb676d538e6b8b4f49d4358f2eedb45cfdeeb0
Maciej Koczot <mefiuea@gmail.com>
2022-06-22 10:46:14 +0200

    add custom permission

--- posts_app/views.py
+++ posts_app/views.py
@@ -1,13 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+    # permission_classes = (permissions.AllowAny,)
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
